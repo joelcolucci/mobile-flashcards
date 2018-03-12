16c239041073720de8a3b8643f2d3c1ed5c4ff31
joelcolucci <joelcolucci@gmail.com>
2018-03-12 15:14:50 -0400

    feat: implement create, read_all action handling

--- reducers/deckReducer.js
+++ reducers/deckReducer.js
@@ -10,10 +10,15 @@ const initialState = {};
 const deckReducer = (previousState=initialState, action) => {
   switch (action.type) {
     case DECK_CREATE_SUCCESS:
-      return previousState;
+      return {
+        ...previousState,
+        [action.title]: {}
+      };
 
     case DECK_READ_ALL_SUCCESS:
-      return previousState;
+      return {
+        ...action.decks
+      };
 
     case DECK_READ_SUCCESS:
       return previousState;
