e16ba16ee43cb9030f822372f037f47ac044b8d1
joelcolucci <joelcolucci@gmail.com>
2018-03-13 11:52:01 -0400

    refactor: use scrollable FlatList

--- screens/AppHomeScreen.js
+++ screens/AppHomeScreen.js
@@ -1,28 +1,44 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { FlatList, Text, View, Button } from 'react-native';
 import { connect } from 'react-redux';
 
 import { readAllDecks } from '../actions/deckActions';
-import DeckList from '../components/DeckList';
 
 
 class AppHomeScreen extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.renderDeckItem = this.renderDeckItem.bind(this);
+  }
+
   componentDidMount() {
     this.props.dispatch(readAllDecks());
+  }
+
+  renderDeckItem({item}) {
+    return (
+      <View>
+        <Text>{item.title}</Text>
+        <Text>{item.questions.length}</Text>
+        <Button
+          title="Go to Details"
+          onPress={() => this.props.navigation.navigate('DeckDetails', {deckId: item.title})} />
+      </View>
+    );
   }
 
   render() {
     return (
       <View>
         <Text>This is the Home view</Text>
-        <DeckList
-          decks={this.props.decks}
-          navigation={this.props.navigation} />
+        <FlatList
+          data={this.props.decks}
+          renderItem={this.renderDeckItem} />
       </View>
     );
   }
 }
-
 
 function mapStateToProps(state, ownProps) {
   let decks = state.deckReducer;
