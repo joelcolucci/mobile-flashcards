b950f9742666b295044e2938033854f7905ff37f
joelcolucci <joelcolucci@gmail.com>
2018-03-14 09:18:04 -0400

    feat: load deck data into QuizScreen comp

--- screens/QuizScreen.js
+++ screens/QuizScreen.js
@@ -1,8 +1,17 @@
 import React from 'react';
 import { Text } from 'react-native';
+import { connect } from 'react-redux';
+
+import { readDeck } from '../actions/deckActions';
 
 
 class QuizScreen extends React.Component {
+  componentDidMount() {
+    let { deckId } = this.props.navigation.state.params;
+
+    this.props.dispatch(readDeck(deckId));
+  }
+
   render() {
     let { deckId } = this.props.navigation.state.params;
     return (
@@ -12,4 +21,17 @@ class QuizScreen extends React.Component {
 }
 
 
-export default QuizScreen;
+function mapStateToProps(state, ownProps) {
+  let decks = state.deckReducer;
+
+  let { deckId } = ownProps.navigation.state.params;
+
+  let deck = decks[deckId];
+
+  return {
+    ...deck
+  };
+}
+
+
+export default connect(mapStateToProps)(QuizScreen);
