42d11003e38bcc78aec62c9e40695bf5c96c5887
joelcolucci <joelcolucci@gmail.com>
2018-03-14 10:05:48 -0400

    feat: implement Question comp w/ req functionality

---
+++ components/Question.js
@@ -0,0 +1,60 @@
+import React from 'react';
+import { Button, Text, View } from 'react-native';
+
+
+class Question extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      showQuestion: true
+    };
+
+    this.handleToggleShowQuestion = this.handleToggleShowQuestion.bind(this);
+  }
+
+  handleToggleShowQuestion() {
+    this.setState((previousSate) => ({
+      showQuestion: !previousSate.showQuestion
+    }));
+  }
+
+  render() {
+    let { showQuestion } = this.state;
+    let mainContent;
+    if (showQuestion) {
+      mainContent = (
+        <View>
+          <Text>{this.props.question}</Text>
+          <Button
+            title="Show Answer"
+            onPress={this.handleToggleShowQuestion} />
+        </View>
+      );
+    } else {
+      mainContent = (
+        <View>
+          <Text>{this.props.answer}</Text>
+          <Button
+            title="Show Question"
+            onPress={this.handleToggleShowQuestion} />
+        </View>
+      );
+    }
+
+    return (
+      <View>
+        {mainContent}
+        <Button
+          title="Correct"
+          onPress={this.props.onCorrect} />
+        <Button
+          title="Incorrect"
+          onPress={this.props.onIncorrect} />
+      </View>
+    );
+  }
+}
+
+
+export default Question;
