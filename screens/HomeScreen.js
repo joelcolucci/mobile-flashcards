9896ea1bbd6e4db96ca0f7ff3623044fef5d1fc2
joelcolucci <joelcolucci@gmail.com>
2018-06-13 14:34:25 -0400

    refactor: gut home screen

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -1,59 +1,26 @@
+import PropTypes from 'prop-types';
 import React from 'react';
-import { FlatList, Text, View, Button } from 'react-native';
+import { View, Button } from 'react-native';
 import { connect } from 'react-redux';
 
-import { readAllDecks } from '../actions/deckActions';
+import Heading from '../components/Heading';
 import { clearAsyncStorage } from '../utilities/StorageAPI';
 
-
 class HomeScreen extends React.Component {
-  constructor(props) {
-    super(props);
-
-    this.renderDeckItem = this.renderDeckItem.bind(this);
-  }
-
-  componentDidMount() {
-    this.props.dispatch(readAllDecks());
-  }
-
-  renderDeckItem({item}) {
-    return (
-      <View>
-        <Text>{item.id}</Text>
-        <Text>{item.title}</Text>
-        <Text>{item.cards.length}</Text>
-        <Button
-          title="Go to Details"
-          onPress={() => this.props.navigation.navigate('DeckDetails', {deckId: item.id})} />
-      </View>
-    );
-  }
-
   render() {
     return (
       <View>
-        <Text>This is the Home view</Text>
+        <Heading>Mobile flashcards</Heading>
         <Button
           title="Clear AsyncStorage"
           onPress={() => clearAsyncStorage()} />
-
-        <FlatList
-          data={this.props.decks}
-          renderItem={this.renderDeckItem} />
       </View>
     );
   }
 }
 
-function mapStateToProps(state, ownProps) {
-  let decks = state.deckReducer;
-  return {
-    decks: Object.keys(decks).map((key) => {
-      return decks[key];
-    })
-  };
-}
+HomeScreen.propTypes = {
+  dispatch: PropTypes.func
+};
 
-
-export default connect(mapStateToProps)(HomeScreen);
+export default connect()(HomeScreen);
